981e73a14780564f5b5e23825e06964084f669a1
Cody <wzhsemail@gmail.com>
2018-06-09 19:56:47 +0800

    修改数据库错误

--- app/src/main/java/com/coolweather/android/db/City.java
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -6,6 +6,7 @@ public class City extends DataSupport {
     private int id;
     private String cityName;
     private int cityCode;
+    private int provinceId;
 
     public int getId() {
         return id;
@@ -27,5 +28,13 @@ public class City extends DataSupport {
 
     public void setCityName(String cityName) {
         this.cityName = cityName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
     }
 }
--- app/src/main/java/com/coolweather/android/db/County.java
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -6,6 +6,7 @@ public class County extends DataSupport {
     private int id;
     private String countyName;
     private String weatherId;
+    private int cityId;
 
     public int getId() {
         return id;
@@ -30,4 +31,13 @@ public class County extends DataSupport {
     public void setWeatherId(String weatherId) {
         this.weatherId = weatherId;
     }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
 }
